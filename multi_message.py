5aa1fbaefd38a8c1c5a1bb8dadd7799935ef6431
kirtan <kjanghan2004@gmail.com>
2023-01-11 23:17:42 +0530

    new python code

--- multi_message.py
+++ multi_message.py
@@ -4,13 +4,19 @@ import time
 
 time.sleep(5)
 for i in range(100):
-    # pyautogui.press("y")
-    # pyautogui.press("u")
-    # pyautogui.press("g")
-    # pyautogui.press("enter")
+    pyautogui.press("h")
+    pyautogui.press("e")
+    pyautogui.press("l")
+    pyautogui.press("l")
+    pyautogui.press("o")
+    pyautogui.press("enter")
    
    
    
     # x,y cordination
     # pyautogui.click(658,711)
     # pyautogui.click(532,711)
+
+
+
+
