88f4452d2227e7036daa9cb7d26f30409efb74da
kirtan <kjanghan2004@gmail.com>
2023-01-13 23:36:42 +0530

    new python code

---
+++ open_website.py
@@ -0,0 +1,33 @@
+
+# import urllib
+# import re
+# url=urllib.urlopen("https://amazon.com").read()
+
+# re.findall("Shopping", url)
+# ['Shopping']
+
+
+# url.find("Shopping")
+
+
+# import urllib.request as ak
+
+# webp=ak.urlopen("https://amazon.com").read()
+
+# webp.find("the_word")
+
+# from amazon.api import AmazonAPI
+
+# access_key = 'YOUR_ACCESS_KEY'
+# secret_key = 'YOUR_SECRET_KEY'
+# associate_tag = 'YOUR_ASSOCIATE_TAG'
+
+# amazon = AmazonAPI(access_key, secret_key, associate_tag)
+
+# products = amazon.search(Keywords='fire stick', SearchIndex='All')
+
+# for i, product in enumerate(products):
+#     print(i, product.title)
+
+
+
