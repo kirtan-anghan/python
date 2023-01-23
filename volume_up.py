6a7c5fbfef5a355ea9c509113def27d4e54d71f0
kirtan <kjanghan2004@gmail.com>
2023-01-23 11:34:17 +0530

    vloume up

---
+++ volume_up.py
@@ -0,0 +1,11 @@
+# set volume output volume 100
+
+
+import applescript
+
+applescript.applescript("set volume output volume 100").run()
+
+
+import osascript
+
+osascript.osascript("set volume output volume 100")
